ffc37b8a27707c52b33f4eb0211b47e42c558023
BenTanYK <tanyk849@gmail.com>
2025-10-29 21:00:19 +0000

    Define public API

--- plkit/__init__.py
+++ plkit/__init__.py
@@ -2,9 +2,21 @@
 
 import importlib.metadata
 
+from .read_orders import read_order, extract_orders
+from .generate_order_form import (
+    generate_product_order,
+    generate_product_personalisations
+)
+
 try:
     __version__ = importlib.metadata.version("plkit")
 except importlib.metadata.PackageNotFoundError:  # pragma: no cover
     __version__ = "0+unknown"
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "read_order",
+    "extract_orders",
+    "generate_product_order",
+    "generate_product_personalisations",
+]
